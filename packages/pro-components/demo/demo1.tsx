73abd4f08d287c18851f033bc634c884201c929f
SKY-MOBI\weiming.chen <weiming.chen@firesoon.com>
2022-08-31 20:19:33 +0800

    测试发布

--- packages/pro-components/demo/demo1.tsx
+++ packages/pro-components/demo/demo1.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { EchartsMap } from '@cwm/pro-components';
 
 const index = () => {
-  return <EchartsMap></EchartsMap>;
+  return <EchartsMap>1111</EchartsMap>;
 };
 
 export default index;
