1334c61fb0b63a511e14a4bf5c38e8a5933efcbc
954244597 <954244597@qq.com>
2022-09-01 14:19:27 +0800

    测试更改git账户

--- packages/pro-components/demo/demo1.tsx
+++ packages/pro-components/demo/demo1.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { EchartsMap } from '@cwm/pro-components';
 
 const index = () => {
-  return <EchartsMap>1111</EchartsMap>;
+  return <EchartsMap>111122</EchartsMap>;
 };
 
 export default index;
