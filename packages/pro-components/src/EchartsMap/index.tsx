10c4a8fa96d4307917741555b6a0a5070d65ea74
954244597 <954244597@qq.com>
2022-10-19 17:42:03 +0800

    feat:测试组件

--- packages/pro-components/src/EchartsMap/index.tsx
+++ packages/pro-components/src/EchartsMap/index.tsx
@@ -1,13 +1,9 @@
 import React, { useEffect } from 'react';
 import ReactECharts from 'echarts-for-react';
 import { IFullScreenCardProps } from './interface';
-import './china.tsx'
 
 const Page: React.FC<IFullScreenCardProps> = ({ label, options }) => {
-  useEffect(() => {
-   
-  
-  }, []);
+  useEffect(() => {}, []);
   return <ReactECharts option={options} style={{ height: 500 }} />;
 };
 
--- packages/pro-components/src/demo/demo1.tsx
+++ packages/pro-components/src/demo/demo1.tsx
@@ -1,50 +1,36 @@
 import React from 'react';
 import { EchartsMap } from '../index';
 
-const options = {
-  tooltip: {
-    show: false,
-  },
+const option = {
   title: {
-    top: 20,
-    // text: '用户注册区域展示',
-    subtext: '',
-    x: 'center',
-    textStyle: {
-      color: '#000',
+    text: 'ECharts 入门示例',
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+      dataZoom: {},
+      restore: {},
     },
   },
-  geo: {
-    map: 'china',
-    roam: true,
-    zoom: 1.23,
-    center: [105, 36],
-    showLegendSymbol: false, // 存在legend时显示
-    label: {
-      normal: {
-        show: true,
-        fontSize: '12',
-        color: '#FFF',
-      },
-      emphasis: {
-        fontSize: '12',
-        color: '#FFF',
-      },
-    },
-    itemStyle: {
-      normal: {
-        borderColor: '#4C63E5', // 地图边框颜色
-        borderWidth: 2, // 地图边框大小
-        areaColor: '#050934', // 各个省份颜色
-      },
-      emphasis: {
-        areaColor: '#050934', // hover颜色
-      },
-    },
+  tooltip: {},
+  legend: {
+    data: ['销量'],
   },
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'line',
+      data: [5, 20, 36, 10, 10, 20],
+    },
+  ],
 };
+
 const index = () => {
-  return <EchartsMap options={options} />;
+  return <EchartsMap options={option} />;
 };
 
 export default index;
